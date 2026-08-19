2bf4bf4cb07856e1c337e26e1fa9df6ddad023af
karl <karlglauser@gmail.com>
2026-08-19 08:32:37 -0300

    Add files via upload

---
+++ estrutura encadeada.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int dia;
+	cout << "digite o numero do dia da semana ( 1 a 7): ";
+	cin >> dia;
+	
+	if(dia >= 1 && dia <= 7){
+		if (dia == 1){
+			cout << "domingo" << endl;
+		} else if (dia == 2) {
+			cout << "segunda-feira" << endl;
+		} else if (dia == 3){
+			cout << "terça-feira" << endl;
+		} else if (dia == 4){
+			cout << "quarta-feira" << endl;
+		} else if (dia == 5){
+			cout << "quinta-feira" << endl;
+		} else if (dia == 6){
+			cout << "sexta-feira" << endl;
+		} else if (dia == 7){
+			cout << "sabado" << endl;
+		}
+
+	} else {
+		cout << "numero do dia invalido." << endl;
+	}
+	return 0;
+}
