c918959e46a181132fc9e13a195e4cbc5d36dcb1
karl <karlglauser@gmail.com>
2026-08-19 09:06:53 -0300

    Add files via upload

---
+++ aula-3/operador ternario.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int numero;
+	
+	cout << "digite um numero: ";
+	cin >> numero;
+	
+	string paridade = (numero % 2 == 0) ? "par" : "impar";
+	
+	cout << " o numero " << numero << " e " << paridade << endl;
+	
+	return 0;
+}
