4dcea18081ea1b487c28fcdf449923d597093263
karl <karlglauser@gmail.com>
2026-08-19 11:14:24 -0300

    Add program to check if a number is positive, negative, or zero

---
+++ aula-2/desafio aula 2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double numero;
+
+    cout << "Digite um numero: ";
+    cin >> numero;
+
+    if (numero > 0) {
+        cout << "O numero e positivo." << endl;
+    } 
+    else if (numero < 0) {
+        cout << "O numero e negativo." << endl;
+    } 
+    else {
+        cout << "O numero e zero." << endl;
+    }
+
+    return 0;
+}
